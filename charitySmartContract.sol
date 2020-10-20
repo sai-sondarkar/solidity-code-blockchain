8619d7a70eca9da6842369f2b806b670de4df273
prabhpreet-singh <kamaljitsingh.singh70@gmail.com>
2020-10-20 05:50:25 +0530

    Update charitySmartContract.sol

--- charitySmartContract.sol
+++ charitySmartContract.sol
@@ -8,31 +8,49 @@ contract charitySmartContract{
         owner = msg.sender;
     }
     
-    function recieveEthers() public payable {
-    }
-    function withdrawAllEthers() public{
+    mapping(address=> uint) public check_transactions;
+//  To check the transactions by address that how much who has donated
+    
+    modifier check_owner{
+ //     modifier will help to give as many functions the property of underlying task no need to repeat the task again and again  
         require(msg.sender == owner, "You Cannot Withdraw All ethers Coz you are not the owner");
+        _;
+    }
+    
+    modifier check_pause{
+ //     modifier will help to give as many functions the property of underlying task no need to repeat the task again and again  
         require(!isPause, "Contract is Paused, we cnt withdraw the money");
+        _;
+    }
+
+    function recieveEthers() public payable {
+         check_transactions[msg.sender] = msg.value ;
+        
+    }
+    function withdrawAllEthers() public check_pause check_owner{
+//       require(msg.sender == owner, "You Cannot Withdraw All ethers Coz you are not the owner");
+//       require(!isPause, "Contract is Paused, we cnt withdraw the money");
         address payable to = msg.sender;
         to.transfer(address(this).balance);
     }
     
-    function setPause( bool _ispause) public{
-        require(msg.sender == owner, "You Dont have access to this function");
+    function setPause( bool _ispause) public check_owner{
+//        require(msg.sender == owner, "You Dont have access to this function");
         isPause = _ispause;
     }
     
-    function setNewOwner( address _newOwner) public{
-        require(msg.sender == owner, "You Dont have access to this function");
-        require(!isPause, "Contract is Paused, we cnt withdraw the money");
+    function setNewOwner( address _newOwner) public check_pause check_owner{
+//        require(msg.sender == owner, "You Dont have access to this function");
+//        require(!isPause, "Contract is Paused, we cnt withdraw the money");
         owner = _newOwner;
     }
     
-    function terminateThisSmartContract() public{
-        require(msg.sender == owner, "You Dont have access to this function");
-        require(!isPause, "Contract is Paused, we cnt withdraw the money");
+    function terminateThisSmartContract() public check_pause check_owner{
+//        require(msg.sender == owner, "You Dont have access to this function");
+//        require(!isPause, "Contract is Paused, we cnt withdraw the money");
         address payable to = msg.sender;
         selfdestruct(to);
     }
+    
     
 }
